aefbd1960deda5e48bbab3885d5b949225a069c1
AMate@inno.tech <AMate@inno.local>
2025-11-21 02:01:51 +0500

    вычисление и вывод в консоль суммы, разности, произведения и частного  чисел

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите первое число: ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.println("Введите второе число: ");
+        int secondNumber = scanner.nextInt();
+
+        int sum = firstNumber + secondNumber;
+        int difference = firstNumber - secondNumber;
+        int product = firstNumber * secondNumber;
+        double quotient = (double) firstNumber / secondNumber; // Преобразуем в double для точности
+
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + difference);
+        System.out.println("Произведение: " + product);
+        System.out.println("Частное: " + quotient);
+
+        scanner.close();
+    }
+}
